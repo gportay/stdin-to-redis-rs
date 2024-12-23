6974457f3072d1db64bd75bb62d993f907f169c7
Gaël PORTAY <gael.portay@rtone.fr>
2024-12-23 23:49:14 +0100

    Read lines from stdin until EOF

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// SPDX-License-Identifier: LGPL-2.1-only
+// SPDX-FileCopyrightText: Copyright 2024 Gaël PORTAY
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer)? > 0 {
+        let line = std::mem::take(&mut buffer);
+        print!("{line}");
+    }
+    Ok(())
 }
