b3365552c9cf639d46d50d01599a23231cc05585
Gaël PORTAY <gael.portay@rtone.fr>
2024-12-24 00:59:37 +0100

    Save line to Redis

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: LGPL-2.1-only
 // SPDX-FileCopyrightText: Copyright 2024 Gaël PORTAY
+use redis::Commands;
 use std::io;
 
 fn main() -> io::Result<()> {
+    let mut conn = redis::Client::open("redis://:@localhost")
+        .expect("Invalid connection URL")
+        .get_connection()
+        .expect("failed to get_connection()");
     let mut buffer = String::new();
     while io::stdin().read_line(&mut buffer)? > 0 {
         let line = std::mem::take(&mut buffer);
+        let _: () = conn.set("line", line).expect("failed to set('line')");
+        let line: String = conn.get("line").expect("failed to get('line')");
         print!("{line}");
     }
     Ok(())
